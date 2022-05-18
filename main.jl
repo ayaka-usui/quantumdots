19a055fdc96097a13c12d30a1cf38ccb12dd9acd
Ayaka Usui <ayaka.usui@oist.jp>
2022-05-18 15:35:06 +0200

    Update main.jl

--- main.jl
+++ main.jl
@@ -238,6 +238,90 @@ function createH_Deltaepsilon!(K::Int64,W::Int64,numvari::Int64,betaL::Float64,b
 
 end
 
+function calculatequantities3(K::Int64,W::Int64,betaL::Float64,betaR::Float64,GammaL::Float64,GammaR::Float64,muL::Float64,muR::Float64,tf::Float64,Nt::Int64)
+
+    # Hamiltonian
+    matH = spzeros(Float64,K*2+1,K*2+1)
+    createH!(K,W,betaL,betaR,GammaL,GammaR,matH)
+
+    # Hamiltonian is hermitian
+    matH = Hermitian(Array(matH))
+    val_matH, vec_matH = eigen(matH)
+    invvec_matH = inv(vec_matH)
+
+    # time
+    time = LinRange(0.0,tf,Nt)
+
+    # correlation matrix
+    # at initial
+    C0 = zeros(Float64,K*2+1)
+    C0[1] = 0.0 + 1e-15 # n_d(0) # make it not 0 exactly to avoid 0.0 log 0.0 = NaN
+    for kk = 1:K
+        C0[1+kk] = 1.0/(exp((matH[1+kk,1+kk]-muL)*betaL)+1.0)
+        C0[1+K+kk] = 1.0/(exp((matH[1+K+kk,1+K+kk]-muR)*betaR)+1.0)
+    end
+    C0 = diagm(C0)
+
+    # epsilonLR = zeros(ComplexF64,K*2+1)
+    epsilonLR = diag(matH)
+
+    Ct = zeros(ComplexF64,K*2+1,K*2+1)
+    dCt = zeros(ComplexF64,K*2+1)
+    Ct_E = zeros(ComplexF64,K*2,K*2)
+    val_Ct_E = zeros(ComplexF64,K*2)
+
+    S_sys = zeros(ComplexF64,Nt)
+
+    S_E = zeros(ComplexF64,Nt)
+    S_alphak = zeros(ComplexF64,Nt)
+    I_SE = zeros(ComplexF64,Nt)
+    betaQL = zeros(ComplexF64,Nt)
+    betaQR = zeros(ComplexF64,Nt)
+    sigma = zeros(ComplexF64,Nt)
+    Drel = zeros(ComplexF64,Nt)
+    I_env = zeros(ComplexF64,Nt)
+
+    for tt = 1:Nt
+
+        Ct .= vec_matH*diagm(exp.(1im*val_matH*time[tt]))*invvec_matH
+        Ct .= Ct*C0
+        Ct .= Ct*vec_matH*diagm(exp.(-1im*val_matH*time[tt]))*invvec_matH
+
+        # observational entropy
+        S_sys[tt] = -Ct[1,1]*log(Ct[1,1]) - (1-Ct[1,1])*log(1-Ct[1,1])
+        
+
+
+
+        Ct_E .= Ct[2:end,2:end]
+        val_Ct_E .= eigvals(Ct_E)
+        vNE_E[tt] = - sum(val_Ct_E.*log.(val_Ct_E)) - sum((1.0 .- val_Ct_E).*log.(1.0 .- val_Ct_E))
+
+        # I_SE
+        I_SE[tt] = vNE_sys[tt] - vNE_sys[1] + vNE_E[tt] - vNE_E[1]
+
+        # heat
+        dCt .= diag(Ct - C0)
+        betaQL[tt] = -sum(dCt[2:K+1].*(epsilonLR[2:K+1] .- muL))*betaL
+        betaQR[tt] = -sum(dCt[K+2:2*K+1].*(epsilonLR[K+2:2*K+1] .- muR))*betaR
+
+        # entropy production
+        sigma[tt] = vNE_sys[tt] - vNE_sys[1] - betaQL[tt] - betaQR[tt]
+
+        # relative entropy
+        Drel[tt] = - betaQL[tt] - betaQR[tt] - (vNE_E[tt] - vNE_E[1])
+
+        # mutual information describing the intraenvironment correlations
+        diag_Ct_E = diag(Ct_E)
+        vNE_alphak[tt] = - sum(diag_Ct_E.*log.(diag_Ct_E)) - sum((1.0 .- diag_Ct_E).*log.(1.0 .- diag_Ct_E))
+        I_env[tt] = vNE_alphak[tt] - vNE_E[tt]
+
+    end
+
+    return time, vNE_E.-vNE_E[1], I_SE, betaQL, betaQR, sigma, Drel, I_env
+
+end
+
 function calculatequantities2(K::Int64,W::Int64,numvari::Int64,betaL::Float64,betaR::Float64,GammaL::Float64,GammaR::Float64,muL::Float64,muR::Float64,tf::Float64,Nt::Int64)
 
     # Hamiltonian
